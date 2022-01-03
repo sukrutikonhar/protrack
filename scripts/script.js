aef6e8a3809cd41890f4aabccec46bdd777cffc3
Sukruti Konhar <52907944+sukrutikonhar@users.noreply.github.com>
2022-01-03 15:07:16 +0530

    second commit

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+$('.carousel').carousel({
+  interval: 1000,
+  pause: 'hover'
+});
+
+
+$('#slider').owlCarousel({
+  loop:true,
+  margin:15,
+  nav:false,
+  autoplay:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
